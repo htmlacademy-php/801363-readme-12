e0e63ad6e6aea45f8c91f0a79ee8f17379fb6551
RobinStone <vohuanrok@mail.ru>
2020-04-25 23:24:59 +0300

    Запрос формирование

--- post.php
+++ post.php
@@ -1,2 +1,7 @@
 <?php
+$link = open_DB();
+q($link, "
+SELECT * FROM `post` WHERE `id` = 1
+");
+
 print(include_template('post-item.php', ['main'=>$main, 'is_auth'=>$is_auth, 'user_name'=>$user_name]));
