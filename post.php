326a964b8ef1106c41b611699ac80243fca37896
RobinStone <vohuanrok@mail.ru>
2020-04-27 12:03:16 +0300

    Исправлено. Устранена возможность запуска скрипта post.php без параметра $GET['id']

--- post.php
+++ post.php
@@ -3,42 +3,44 @@ include_once ('./functions.php');
 include_once ('./helpers.php');
 
 
-if(isset($_GET['id']) )
-$link = open_DB();
-$ask = q($link, "
+if(isset($_GET['id']) ) {
+    $link = open_DB();
+    $ask = q($link, "
 SELECT post.*, content_type.id AS CONTENT_ID, content_type.class, users.id AS USER_ID, users.img AS AVATAR, users.login FROM `post`
 LEFT JOIN `content_type` ON
 post.id_type_content = content_type.id
 LEFT JOIN `users` ON
 post.id_user = users.id
-WHERE post.id = ".(int)$_GET['id']."
+WHERE post.id = " . (int)$_GET['id'] . "
 ");
 
-if($ask->num_rows) {
-    $row = $ask->fetch_assoc();
+    if ($ask->num_rows) {
+        $row = $ask->fetch_assoc();
 
-$ask = q($link, "
-SELECT COUNT(*) FROM `post` WHERE `id_user` = ".(int)$row['USER_ID']."
+        $ask = q($link, "
+SELECT COUNT(*) FROM `post` WHERE `id_user` = " . (int)$row['USER_ID'] . "
 ");
 
-if($ask->num_rows) {
-    $row['count_pub'] = $ask->fetch_assoc()['COUNT(*)'];
-}
+        if ($ask->num_rows) {
+            $row['count_pub'] = $ask->fetch_assoc()['COUNT(*)'];
+        }
 
-$ask = q($link, "
-SELECT COUNT(*) FROM `subscriptions` WHERE `id_author` = ".(int)$row['USER_ID']."
+        $ask = q($link, "
+SELECT COUNT(*) FROM `subscriptions` WHERE `id_author` = " . (int)$row['USER_ID'] . "
 ");
 
-    if($ask->num_rows) {
-        $row['count_subs'] = $ask->fetch_assoc()['COUNT(*)'];
-    }
+        if ($ask->num_rows) {
+            $row['count_subs'] = $ask->fetch_assoc()['COUNT(*)'];
+        }
 
 //    wtf($row);
 
-    $post = include_template('content_'.$row['class'].'.php', ['itm'=>$row]);
-    print(include_template('post-item.php', ['itm'=>$row, 'post'=>$post]));
+        $post = include_template('content_' . $row['class'] . '.php', ['itm' => $row]);
+        print(include_template('post-item.php', ['itm' => $row, 'post' => $post]));
 
+    } else {
+        echo '404';
+    }
 } else {
-    echo '404';
+    echo 'Не верно заданы параметры $GET["id"]';
 }
-
